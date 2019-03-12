c1945070fbeb5e335b23a27885e097b83aeb88d5
Becky Arrowsmith <becky.arrowsmith@googlemail.com>
2019-03-12 15:52:30 +0000

    Webpack implementation is working

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,13 @@
-console.log('Hello from Dockerised Prototype kit.js')
-console.log('Im inside the prototype folder')
+import exampleModule from './exampleModule.js'
+
+function entryPoint () {
+  const msg = "I'm the entry point!"
+  const element = document.getElementById('hello-world')
+  const text = document.createTextNode(`${msg}`)
+
+  element.appendChild(text)
+  console.log(`${msg}`)
+}
+
+entryPoint()
+exampleModule()
